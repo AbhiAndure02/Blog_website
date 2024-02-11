0689696da01ed057f9a57f5104c0919e10ca31bb
Abhishek  Andure <91086876+AbhiAndure02@users.noreply.github.com>
2024-02-11 12:00:25 +0530

    update post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -90,7 +90,7 @@ export default function PostPage() {
       </div>
       <CommentSection postId={post._id} />
 
-      <div className='flex flex-col justify-center items-center mb-5'>
+      <div className='flex flex-col gap-10'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
           {recentPosts &&
